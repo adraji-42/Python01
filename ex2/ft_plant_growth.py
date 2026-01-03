b0250604abcf5b96429cd11ceda9add4081ed1e4
Adam Raji <adraji@e1r2p13.1337.ma>
2026-01-03 18:18:58 +0100

    change name of variable

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -56,8 +56,8 @@ def ft_simulat_week(plants: list):
 
 def main():
     """Entry point of the script to initialize and run the simulation."""
-    rose = Plant("Rose", 25, 30)
-    ft_simulat_week([rose])
+    Rose = Plant("Rose", 25, 30)
+    ft_simulat_week([Rose])
 
 
 if __name__ == "__main__":
